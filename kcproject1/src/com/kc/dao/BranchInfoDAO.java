d2031d9e012b9605aed0e7402ff00baf255fed2e
hhheunji <skks308@gmail.com>
2018-12-12 14:06:15 +0900

    BranchInfo은지꺼

--- kcproject1/src/com/kc/dao/BranchInfoDAO.java
+++ kcproject1/src/com/kc/dao/BranchInfoDAO.java
@@ -2,14 +2,11 @@ package com.kc.dao;
 
 import java.util.List;
 
+import com.kc.exception.NotFoundException;
 import com.kc.vo.BranchInfo;
 
 public interface BranchInfoDAO {
-	
-	public List<BranchInfo> selectAll();
-	
-	public BranchInfo selectByBC(String branch_code);
-	
-	public void createBF(BranchInfo branchinfo);
-	
+	List<BranchInfo> selectAll();
+	BranchInfo selectByCode(String branch_code) throws NotFoundException;
+
 }
