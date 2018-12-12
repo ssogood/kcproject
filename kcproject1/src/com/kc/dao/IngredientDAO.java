4d51fe5ba598654391fc069389fe82fede0ffb28
cocoory <cocoory@gmail.com>
2018-12-12 13:46:47 +0900

    NotFoundException

--- kcproject1/src/com/kc/dao/IngredientDAO.java
+++ kcproject1/src/com/kc/dao/IngredientDAO.java
@@ -3,9 +3,15 @@ package com.kc.dao;
 import java.util.List;
 
 import com.kc.vo.IngredientInfo;
+import com.kc.exception.NotFoundException;
 
 public interface IngredientDAO {
 	List<IngredientInfo> selectAll();
 	List<IngredientInfo> selectByName(String ingred_name);
 
+	//balju
+	List<IngredientInfo> selectAllbaljuingr();
+	IngredientInfo selectByNobaljuingr(int ingred_no) throws NotFoundException ;
+	List<IngredientInfo> selectByNamebaljuingr(String ingred_name);
+	IngredientInfo selectByNo(int ingred_no);
 }
