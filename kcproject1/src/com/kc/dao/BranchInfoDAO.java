3e9bc0f5b37670a1c3bf4eb4d1d1ab2fb9f3297c
cocoory <cocoory@gmail.com>
2018-12-12 13:45:54 +0900

    NotFoundException

--- kcproject1/src/com/kc/dao/BranchInfoDAO.java
+++ kcproject1/src/com/kc/dao/BranchInfoDAO.java
@@ -2,11 +2,14 @@ package com.kc.dao;
 
 import java.util.List;
 
-import com.kc.exception.NotFoundException;
 import com.kc.vo.BranchInfo;
 
 public interface BranchInfoDAO {
-	List<BranchInfo> selectAll();
-	BranchInfo selectByCode(String branch_code) throws NotFoundException;
-
+	
+	public List<BranchInfo> selectAll();
+	
+	public BranchInfo selectByBC(String branch_code);
+	
+	public void createBF(BranchInfo branchinfo);
+	
 }
