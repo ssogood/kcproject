a9ff0016a6be3b38a4c74450f3a3c68b85702c8f
cocoory <cocoory@gmail.com>
2018-12-12 13:46:32 +0900

    import

--- kcproject1/src/com/kc/dao/BranchInfoDAOOracle.java
+++ kcproject1/src/com/kc/dao/BranchInfoDAOOracle.java
@@ -1,51 +1,95 @@
 package com.kc.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.kc.exception.NotFoundException;
 import com.kc.vo.BranchInfo;
-import com.kc.vo.IngredientInfo;
 
-@Repository("branchDAO")
-public class BranchInfoDAOOracle implements BranchInfoDAO {
+@Repository("branchinfoDAO")
+public class BranchInfoDAOOracle implements BranchInfoDAO{
 
 	@Autowired
 	private SqlSessionFactory sqlSessionFactory;
 	@Autowired
 	private DataSource dataSource;
-	
-	@Override
-	public List<BranchInfo> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
-	public BranchInfo selectByCode(String branch_code) throws NotFoundException {
-		
+	public List<BranchInfo> selectAll() {
+		List<BranchInfo> all = new ArrayList<>();
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		try {
-			BranchInfo branch = sqlSession.selectOne("BranchInfoMapper.selectByCode",branch_code);
-			if(branch==null) {
-				throw new NotFoundException("해당 고객을 찾을 수 없습니다.");
-			}else {
-				return branch;
-			}
+
+			all = sqlSession.selectList("BranchInfoMapper.selectAll");
 		}catch(Exception e) {
-			throw new NotFoundException(e.getMessage());
+			System.err.println(e.getMessage());
+		}
+		finally {
+			sqlSession.close();
+		}
+		return all;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	//1210
+	@Override
+	public BranchInfo selectByBC(String branch_code) {
+		SqlSession ss = sqlSessionFactory.openSession();
+		try {
+			BranchInfo brc = new BranchInfo();
+			brc = ss.selectOne("BranchInfoMapper.selectByBC", branch_code);
+			return brc;
 		}finally {
-			if(sqlSession !=null) {
-				sqlSession.close();
-			}
+			ss.close();
 		}
 	}
+	
+	@Override
+	public void createBF(BranchInfo branchinfo) {
+		SqlSession ss = sqlSessionFactory.openSession();
+		try {
+			ss.update("BranchInfoMapper.createBF", branchinfo);
+		}finally {
+			ss.close();
+		}
+	}
+	
+	
+	
+	
+	/*public static void main(String[] args) {
+		String resource = "config/mybatis-config.xml";
+		InputStream inputStream;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			SqlSession ss = sqlSessionFactory.openSession();
+			
+			String branch_code = "S0001";
+			BranchInfo brc = new BranchInfo();
+			brc = ss.selectOne("BranchInfoMapper.selectByBC", branch_code);
+			System.out.println(brc);
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
 
 }
