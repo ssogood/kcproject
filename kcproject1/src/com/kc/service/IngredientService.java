1b82cdc1045122dd195aa36be627907bda02a936
cocoory <cocoory@gmail.com>
2018-12-12 13:49:44 +0900

    import

--- kcproject1/src/com/kc/service/IngredientService.java
+++ kcproject1/src/com/kc/service/IngredientService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.kc.dao.IngredientDAO;
 import com.kc.vo.IngredientInfo;
+import com.kc.exception.NotFoundException;
 
 @Service
 public class IngredientService {
@@ -16,9 +17,29 @@ public class IngredientService {
 	public List<IngredientInfo> findAll(){
 		return dao.selectAll();
 	}
-	
 	public List<IngredientInfo> findByName(String ingred_name){
 		return dao.selectByName(ingred_name);
+	}
+	
+	
+	
+	
+	//지점에서 쓸거(부분정보)
+	public List<IngredientInfo> findAllingr(){
+		return dao.selectAllbaljuingr();
+	}
+	//지점에서 쓸거(부분정보)
+	public IngredientInfo findByNoingr(int ingred_no) throws NotFoundException {
+		return dao.selectByNobaljuingr(ingred_no);
+	}
+	//지점에서 쓸거(부분정보)
+	public List<IngredientInfo> findByNameingr(String ingred_name) {
+		return dao.selectByNamebaljuingr(ingred_name);
+	}
+	
+	//1210 본사에서쓸거(모든정보)
+	public IngredientInfo findByNo(int ingred_no) throws NotFoundException {
+		return dao.selectByNo(ingred_no);
 	}
 
 }
